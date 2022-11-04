73f6d889668b757981bb22ad397a6cb3c9395e8d
ivdan <ivdanilov96@gmail.com>
2022-11-04 22:11:13 +0300

    repeat decorator

--- service.py
+++ service.py
@@ -1,32 +1,26 @@
 from tax_info import TaxInfo
-from utils import round_up
+from utils import round_up, repeat_on_invalid_input
 
 
+@repeat_on_invalid_input
 def find_annual_income():
-    try:
-        year = int(input("Enter the desired year \n"))
-        info = TaxInfo()
-        result = 0
-        for row in map(lambda x: x.income_value, info.findByDate({"year": year})):
-            result += row
-        print(f"The annual income for {year}'s year is {result}")
-    except ValueError:
-        if input("Try again? Y/N \n") == 'Y':
-            find_annual_income()
+    year = int(input("Enter the desired year \n"))
+    info = TaxInfo()
+    result = 0
+    for row in map(lambda x: x.income_value, info.findByDate({"year": year})):
+        result += row
+    print(f"The annual income for {year}'s year is {result}")
 
 
+@repeat_on_invalid_input
 def moth_tax_value():
-    try:
-        year = int(input("Enter the desired year \n"))
-        month = int(input("Enter the desired month \n"))
-        info = TaxInfo()
-        result = 0
-        for income in map(converted_income(), info.findByDate({"year": year, "month": month})):
-            result += income
-        print(f"For  {month}.{year} you should pay {round_up(result * 0.01)}")
-    except ValueError:
-        if input("Try again? Y/N \n") == 'Y':
-            moth_tax_value()
+    year = int(input("Enter the desired year \n"))
+    month = int(input("Enter the desired month \n"))
+    info = TaxInfo()
+    result = 0
+    for income in map(converted_income(), info.findByDate({"year": year, "month": month})):
+        result += income
+    print(f"For  {month}.{year} you should pay {round_up(result * 0.01)}")
 
 
 def converted_income():
--- utils.py
+++ utils.py
@@ -17,7 +17,27 @@ def transaction(func):
     return wrapper
 
 
+def repeat_on_invalid_input(func):
+    """
+    Useful for functions, which expect user input values
+    If user input unexpected value, he could try input it again
+    """
+
+    def wrapper(*args):
+        try:
+            return func(*args)
+        except ValueError:
+            if input("Invalid input data. Would you like to try again? Y/N \n") == 'Y':
+                wrapper(*args)
+
+    return wrapper
+
+
 def round_up(num: float):
-    """custom summary rounding """
+    """
+    custom summary rounding
+    takes 3 digits after the decimal point and round it in ceil, couse it's better to pay
+     a little more, then have any debt
+    """
     val = num * 1000 % 1000
     return math.ceil(num) if val >= 445 else math.floor(num)
